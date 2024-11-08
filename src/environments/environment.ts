1ab5bd66f47f5e75852e97f3873d1697e20fe8fb
Cesar <llancafilcesar@gmail.com>
2024-11-08 15:31:00 -0300

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   marvelPublicKey: 'c5b00a2c20e02886606b1c3c929bd75e',
-  marvelPrivateKey: 'e85a10339ae2a5f10a67afae7ff2de357d9c2539',
+  marvelPrivateKey: 'insert key',
   marvelBaseUrl: 'https://gateway.marvel.com/v1/public',
 };
